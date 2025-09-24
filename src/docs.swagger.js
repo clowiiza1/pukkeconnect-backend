50a7d5123951c762d6c254cdfef17b22f4c43674
clowiiiza <43036376@mynwu.ac.za>
2025-09-24 14:37:56 +0200

    added post apis

--- src/docs.swagger.js
+++ src/docs.swagger.js
@@ -5,6 +5,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: { title: 'PukkeConnect API', version: '1.0.0' },
+    servers: [{ url: `http://localhost:4000` }],
 
     // Tell Swagger how we handle JWTs
     components: {
@@ -18,8 +19,15 @@ const options = {
   },
 
   // Look for JSDoc comments in route files
-  apis: ['src/modules/**/*.routes.js']
+apis: [
+    'src/modules/**/*.routes.js',
+   'src/modules/**/*.route.js',
+   'src/modules/**/posts.*.js' // belt & braces for your posts file
+  ]
+  
 };
 
 export const swaggerSpec = swaggerJsdoc(options);
+console.log('Swagger paths loaded:', Object.keys(swaggerSpec.paths || {}));
+
 export { swaggerUi };
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import 'dotenv/config';           // loads .env before anything else
 import { env } from './config.js';
 import app from './app.js';
 
+
+
 const server = app.listen(env.port, () => {
   console.log(`API listening on http://localhost:${env.port}`);
   console.log(`Docs: http://localhost:${env.port}/docs`);
@@ -10,9 +12,7 @@ const server = app.listen(env.port, () => {
 // Graceful shutdown
 const shutdown = () => {
   console.log('Shutting down…');
-  server.close(() => {
-    process.exit(0);
-  });
+  server.close(() => process.exit(0));
 };
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
